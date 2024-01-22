2d2564796a350761cda4bc0eb58b2fb77549ebee
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-23 03:05:51 +0530

    2nd Day of My Learning python
    hahahahah!! it is day 2 NOW

---
+++ 2ndcode.py
@@ -0,0 +1,8 @@
+#this is going to be my first comment on python and i don't know what to write
+#so i am just writing anything lol.
+
+print("FIRST COMMENT ON python")
+print(25 , "is the day i born")
+print(True)
+
+print("now it's the second day of my using github and VS Code for\nusing python , so i am excited to use this command to go to the next line.")
