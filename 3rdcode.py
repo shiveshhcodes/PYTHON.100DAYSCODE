f4d81e3abfc8374e3dfa82f5df84e5b01f788d5d
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-24 03:25:09 +0530

    3rd Day Of Code

---
+++ 3rdcode.py
@@ -0,0 +1,14 @@
+a = 1912
+b = 12109
+c = "shivesh richhariya"
+d = False 
+e = None
+print(a+b)
+print(a*b)
+print(a%b)
+
+print ("the type of" , a , "is" , type(a))
+print ("the type of" , b , "is" , type (b))
+print ("the type of", c , "is" , type (c))
+print ("the type of" , d , "is" , type(d))
+print ("the type of" ,e , "is" , type(e))
