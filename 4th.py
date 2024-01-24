c83d34ee664f48d957c32335e70533bda0700d62
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-25 02:39:53 +0530

    calculator using python

---
+++ 4th.py
@@ -0,0 +1,16 @@
+#i am planning to make my first calculator using python , rn.. so let's do it.
+
+a = 1090
+b = 9089
+
+ans1= a+b
+print("the sum of",  a , "and" , b , "is" , ans1 )
+
+ans2= a-b
+print("the subtraction of" , a , "and" , b , "is" , ans2)
+
+ans3= a*b
+print("the multiplication of" , a , "and" , b , "is" , ans3) 
+
+ans4 = a/b
+print("the division of" , a , "and" , b , "is" , ans4)
