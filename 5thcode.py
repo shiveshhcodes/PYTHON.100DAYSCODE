dbe5b1c0a0b79fd1c6da8c01a0c4e73856c8480d
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-27 02:48:09 +0530

    day 05 of my code series

---
+++ 5thcode.py
@@ -0,0 +1,13 @@
+#here we learned about , what is typecasting and how does it works for all.
+
+a = 320
+b = 232
+
+print("the sum of" , a , "and" , b , "is equal to" , a+b)
+
+# now let use these numbers as strings and not as integers
+
+a = "1298"
+b = "2.92" #here value of b is in float.
+
+print ("the sum of" , int(a) , "and" , float(b), "is" ,int(a)+float(b))
