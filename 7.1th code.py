cf4e40c3741471165113584b23be629afccd7a62
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-29 00:31:38 +0530

    it's 7.1 dude (practice code)

---
+++ 7.1th code.py
@@ -0,0 +1,19 @@
+a = input("Hey!! What is your Date & Month : ")
+print ("Oh , nice you born on" , a , "Even I born'ed on same date")
+b = input ("Now , let's do some magic things. Are You Excited or Not?? : " )
+print ("Yeahhhhh!! let's do it then" , a )
+
+shivesh = """ Let me guess about of you few things : 
+1. You are a male
+2. You live in india
+3. You use macbook air 15 inch M2
+"""
+print (shivesh)
+
+RICHHARIYA = ("""
+Shivesh
+CSEIML
+IPS
+""")
+for character in RICHHARIYA:
+    print(character)
