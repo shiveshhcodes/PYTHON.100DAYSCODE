d30f5d36bfca1e2655c0391dc3d0439b888c94b6
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-28 23:56:20 +0530

    day 007

---
+++ 7thcode.py
@@ -0,0 +1,20 @@
+# a = input ("What is your Name?? ")
+# print ('thank you for using this "program"' , a)
+# b = input ("What would you like me to do for you " + a + " ; \"sir/mam\"" ":" ) 
+# print ("will do" , b , "it for you" , a )
+
+st = ("""
+1. MY NAME IS
+
+2. SHIVESH RICHHARIYA
+
+3. I AM A STUDENT OF 3RD YEAR CS
+
+""")
+print (st)
+
+# name = "shivesh"
+# print (name[0])
+print("lets divide characters in \n")
+for character in st :
+  print (character)
