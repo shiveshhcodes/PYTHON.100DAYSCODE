76025b94d20c7260c8a17f4006850eef59fa2216
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-30 23:59:02 +0530

    DAY 008

---
+++ 8thcode.py
@@ -0,0 +1,7 @@
+name = "richhariya"
+namelen = len(name)
+print (namelen) 
+
+print (name[0:5]) #this includes 0 but not 5
+print (name[0:8]) #this includes 0 but not 8
+print (name[3:10]) #this includes 3 but not 10
