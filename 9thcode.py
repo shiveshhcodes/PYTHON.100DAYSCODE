75386c055eef87218c741d1974fb303ec3183c0b
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-01-31 23:57:34 +0530

    9th day

---
+++ 9thcode.py
@@ -0,0 +1,26 @@
+#let's discover various strings of python today.
+
+string1 = " ShIvesH "
+print(string1.upper())
+print(string1.lower())
+
+string2 = " ShIvesH "
+print(string2.strip)
+
+string3 = "Hello Shivesh !!!"
+print(string3.rstrip("!"))
+
+string4 = "Hello Shiveshhh"
+print(string4.replace("Hello" , "Hi"))
+
+string5 = "Namaste Sirji"
+print (string5.split(" "))
+
+string6 = "JAI SHRI RAM"
+print(string6.capitalize())
+
+string7 = "SHIVESH RICHHARIYA"
+print(string7.center(69 , ":"))
+
+string8 = "shibusokoks"
+print(string8.count("s"))
