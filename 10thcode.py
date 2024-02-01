4b0f71e912dc336e56eda55f3b5c644e2b837454
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-01 20:28:31 +0530

    10TH CODE

---
+++ 10thcode.py
@@ -0,0 +1,29 @@
+a = input("What is Your Name?? : ")
+print("\nHi", a, "I hope you are doing good")
+b = input("\nShall we start our program?? (yes/no) : ")
+
+if b.lower() == "yes":
+    print("Let's Rock then")
+
+    s = int(input("\nEnter Your Age " + a + ":"))
+    print("Your Age is:", s)
+
+    if s > 19:
+        print("You can go to college now")
+        ss = input("\nAre you happy now?? (Yes/No): ")
+        if ss.lower() == "yes":
+            print("Good Boy")
+        else:
+            print("\nBad Habit")
+            print("GO AND STUDY, YOU STUPID")
+
+    elif s == 18:  
+        print("I guess you just passed your 12th, you can go to\nCollege from next year.")
+
+    else:
+        print("You cannot go to college now")
+
+else:
+    print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+
+print("Thank you for checking my program.\nHave a Good Day", a, ":)")
