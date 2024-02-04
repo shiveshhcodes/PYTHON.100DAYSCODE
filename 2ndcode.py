d8fd712ac942818285ae88f7191b58ec2033f5f0
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-04 23:46:07 +0530

    Update 2ndcode.py

--- 2ndcode.py
+++ 2ndcode.py
@@ -1,8 +1,8 @@
 #this is going to be my first comment on python and i don't know what to write
 #so i am just writing anything lol.
 
-print("This my first comment statement in python")
-print(25 , "is the day i born")
+print("This is going to be my first comment statement in python")
+print(25 , "is my DOB")
 print(True)
 print("now it's the second day of my using github and VS Code for\nusing python , so i am excited to use this command to go to the next line.")
 print("lets move to now day 3")
