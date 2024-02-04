ca72c2f86175626f03dc5805b03af99f1947cc7f
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-05 03:25:18 +0530

    while loops

---
+++ 13thcode.py
@@ -0,0 +1,32 @@
+i = 129
+while(i<200):
+    print(i)
+    i = i+1
+
+j = input("Hi, What's Your Name, sir? ")
+print("Hello Mr.", j)
+
+i = int(input("Guess the Maximum Number of my range: "))
+
+while i < 38:
+    print("No,", i, "isn't my maximum range")
+    i = int(input("Try again. Guess the Maximum Number of my range: "))
+
+print("Done with the loop thing")
+
+if i >= 38:
+    print(i, "is greater than or equal to 38, that is in my range")
+
+i = 991
+while(i>=90):
+    print(i)
+    i = i-1
+
+i = 2506
+while(i<=3000):
+    print(i)
+    i=i+1
+
+
+
+
