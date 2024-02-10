26e7539d8ed5ac760875c7c138c0c997714effa7
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-10 23:27:58 +0530

    Update 14thcode.py

--- 14thcode.py
+++ 14thcode.py
@@ -1,12 +1,17 @@
-for i in range(16):
-  if(i == 10):
-    print("Skip the iteration")
-    continue
-  print("5 X", i, "=", 5 * i)
+# for i in range(16):
+#   if(i == 10):
+#     print("Skip the iteration")
+#     continue
+#   print("5 X", i, "=", 5 * i)
   
-i = 0
-while True:
-  print(i)
-  i = i + 1
-  if(i%100 == 0):
-    break
+# i = 0
+# while True:
+#   print(i)
+#   i = i + 1
+#   if(i%100 == 0):
+#     break
+
+for i in range(12):
+    print("10 X" , i+1 , "=" , 10* (i+1))
+
+print("THIS LOOP IS ENDED HERE")
