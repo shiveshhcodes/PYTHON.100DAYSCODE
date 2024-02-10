6b10cb5c1591adf0335bf294f9ff9961eeba4488
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-10 23:28:01 +0530

    Update 13thcode.py

--- 13thcode.py
+++ 13thcode.py
@@ -1,31 +1,31 @@
-# i = 129
-# while(i<200):
-#     print(i)
-#     i = i+3
+i = 129
+while(i<200):
+    print(i)
+    i = i+3
 
-# j = input("Hi, What is Your Name ")
-# print("Hello Mr./Mrs.", j)
+j = input("Hi, What is Your Name ")
+print("Hello Mr./Mrs.", j)
 
-# i = int(input("Guess the Maximum Number of my range: "))
+i = int(input("Guess the Maximum Number of my range: "))
 
-# while i < 38:
-#     print("No,", i, "isn't my maximum range")
-#     i = int(input("Try again. Guess the Maximum Number of my range: "))
+while i < 38:
+    print("No,", i, "isn't my maximum range")
+    i = int(input("Try again. Guess the Maximum Number of my range: "))
 
-# print("Done with the loop thing")
+print("Done with the loop thing")
 
-# if i >= 38:
-#     print(i, "is greater than or equal to 38, that is in my range")
+if i >= 38:
+    print(i, "is greater than or equal to 38, that is in my range")
 
-# i = 991
-# while(i>=90):
-#     print(i)
-#     i = i-1
+i = 991
+while(i>=90):
+    print(i)
+    i = i-1
 
-# i = 2506
-# while(i<=3000):
-#     print(i)
-#     i=i+1
+i = 2506
+while(i<=3000):
+    print(i)
+    i=i+1
 
 while True:
     Number=int(input("Enter a positive Number : "))
