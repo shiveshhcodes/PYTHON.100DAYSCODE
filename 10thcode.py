af4f43c636e824b2c124fcd181086a82075cccf2
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-10 23:35:52 +0530

    Update 10thcode.py

--- 10thcode.py
+++ 10thcode.py
@@ -1,15 +1,80 @@
-a = input("Tell Me Your Name?? : ")
-print("\nHi", a, "I hope you are doing fantastic")
-b = input("\nDo You agree to all our T&C's? (yes/no) : ")
+# # a = input("WHAT IS YOUR NAME?? : ")
+# # print("\nHi", a, "I hope you are doing fantastic")
+# # b = input("\nDo You agree to all our T&C's? (yes/no) : ")
+# # c = input("\nDo You READ to all our T&C's? (yes/no) : ")
 
-if b.lower() == "yes":
+# # if c.lower() == "yes" and b.lower() =="yes" :
+# #     print("Let's do it then")
+
+# #     s = int(input("\nTell me your Age??" + a + ":"))
+# #     print("Your Age is :", s )
+
+# #     if s > 19:
+# #         print("You can go to college now , You are adult ")
+# #         ss = input("\nAre you happy now?? (Yes/No): ")
+# #         if ss.lower() == "yes":
+# #             print("Good Boy")
+# #         else:
+# #             print("\nBad Habit")
+# #             print("GO AND STUDY")
+
+# #     elif s == 18:  
+# #         print("I guess you just passed your 12th, you can go to\nApply for college next year buddy.")
+
+# #     else:
+# #         print("You cannot go to college now")
+
+# # else:
+# #     print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+
+# # print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+
+# a = input("WHAT IS YOUR NAME?? : ")
+# print("\nHi", a, "I hope you are doing fantastic")
+
+# b = input("\nDo You agree to all our T&C's? (yes/no) : ")
+# c = input("\nDo You READ to all our T&C's? (yes/no) : ")
+
+# if c.lower() == "yes" and b.lower() == "yes":
+#     print("Let's do it then")
+
+#     s = int(input("\nTell me your Age??" + a + ":"))
+#     print("Your Age is:", s)
+
+#     if s > 19:
+#         print("You can go to college now, You are an adult ")
+#         ss = input("\nAre you happy now?? (Yes/No): ")
+#         if ss.lower() == "yes":
+#             print("Good Boy")
+#         else:
+#             print("\nBad Habit")
+#             print("GO AND STUDY")
+
+#     elif s == 18:
+#         print("I guess you just passed your 12th, you can go to\nApply for college next year buddy.")
+
+#     else:
+#         print("You cannot go to college now")
+
+# else:
+#     print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+
+# print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+
+a = input("WHAT IS YOUR NAME?? : ")
+print("\nHi", a, "I hope you are doing fantastic")
+
+b = input("\nDo You agree to all our T&C's? (yes/no) : ")
+c = input("\nDo You READ to all our T&C's? (yes/no) : ")
+
+if c.lower() == "yes" and b.lower() == "yes":
     print("Let's do it then")
 
     s = int(input("\nTell me your Age??" + a + ":"))
-    print("Your Age is :", s )
+    print("Your Age is:", s)
 
     if s > 19:
-        print("You can go to college now , You are adult ")
+        print("You can go to college now, You are an adult ")
         ss = input("\nAre you happy now?? (Yes/No): ")
         if ss.lower() == "yes":
             print("Good Boy")
@@ -17,13 +82,11 @@ if b.lower() == "yes":
             print("\nBad Habit")
             print("GO AND STUDY")
 
-    elif s == 18:  
+    elif s == 18:
         print("I guess you just passed your 12th, you can go to\nApply for college next year buddy.")
 
     else:
         print("You cannot go to college now")
 
-else:
-    print("Thank you for checking my program.\nHave a Good Day", a, ":)")
+print("\nThank you for checking my program.\nHave a Good Day", a, ":)")
 
-print("Thank you for checking my program.\nHave a Good Day", a, ":)")
