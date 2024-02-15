e179fdfb9a278fac5098e13bd0dd9ba84e524964
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-15 23:56:50 +0530

    Update 7.1th code.py

--- 7.1th code.py
+++ 7.1th code.py
@@ -4,7 +4,7 @@ b = input ("Now , let's do some magic things. Are You Excited or Not?? : " )
 print ("Yeahhhhh!! let's do it then" , a )
 
 shivesh = """ Let me guess about of you few thingsssssss : 
-1. You are a maleee
+1. You are a maleeeEEEE
 2. You live in india
 3. You use macbook air 15 inch M2
 """
