f3e623f730fba41e42ba8b75beb71f764ebf29ae
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-02-21 23:55:21 +0530

    Update 8thcode.py

--- 8thcode.py
+++ 8thcode.py
@@ -2,6 +2,6 @@ name = "richhariya"
 namelen = len(name)
 print (namelen) 
 
-print (name[0:7]) #this includes 0 but not 7
+print (name[0:3]) #this includes 0 but not 7
 print (name[0:5]) #this includes 0 but not 8
 print (name[3:10]) #this includes 3 but not 10
