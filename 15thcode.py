6a96455e1ba0cb8ff35c70ee17c4aebc65a5a1a6
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-04-28 07:34:05 +0530

    Update 15thcode.py

--- 15thcode.py
+++ 15thcode.py
@@ -1,26 +1,46 @@
-def calculateGmean(a, b):
-  mean = (a*b)/(a-b)
-  print(mean)
+# def calculateGmean(a, b):
+#   mean = (a*b)/(a-b)
+#   print(mean)
 
-def isGreater(a, b):
-  if(a>b):
-    print("First number is greaterRRRR")
-  else:
-    print("Second number is greater or equallll")
+# def isGreater(a, b):
+#   if(a>b):
+#     print("First number is greaterRRRR")
+#   else:
+#     print("Second number is greater or equallll")
 
-def isLesser(a, b):
-  pass
+# def isLesser(a, b):
+#   pass
   
 
-a = 9
-b = 8
-isGreater(a, b)
-calculateGmean(a, b)
-# gmean1 = (a*b)/(a+b)
-# print(gmean1)
-c = 8
-d = 74
-isGreater(c, d)
-calculateGmean(c, d)
-# gmean2 = (c*d)/(c+d)
-# print(gmean2)
+# a = 9
+# b = 8
+# isGreater(a, b)
+# calculateGmean(a, b)
+# # gmean1 = (a*b)/(a+b)
+# # print(gmean1)
+# c = 8
+# d = 74
+# isGreater(c, d)
+# calculateGmean(c, d)
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
+
+def calculateMultiply(a,b):
+    multiply = (a*b)
+    print(multiply)
+
+def Caldivide(c,d):
+    divide = (c%d)
+    print(divide)
+
+a = 21 
+b = 10
+
+calculateMultiply(a,b)
+
+c = 12
+d = 10
+
+Caldivide(c,d)
+
+
