50323db54c18cb7b0185002fa7b89ae3f091870c
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-04-28 07:34:07 +0530

    Update 16thcode.py

--- 16thcode.py
+++ 16thcode.py
@@ -2,21 +2,21 @@
 #   print("The average is ", (a + b + c) / 2)
 
 
-def average(*numbers):
-  # print(type(numbers))
-  sum = 0
-  for i in numbers:
-    sum = sum + i
+# def average(*numbers):
+#   # print(type(numbers))
+#   sum = 0
+#   for i in numbers:
+    # sum = sum + i
   # print("Average is: ", sum / len(numbers))
   # return 7
-  return sum / len(numbers)
+  # return sum / len(numbers)
 
 
 # average(4, 6)
 # average(b=9)
 
-c = average(5, 6, 7, 1,8,2)
-print(c)
+# c = average(5, 6, 7, 1,8,2)
+# print(c)
 
 
 # def name(**name):
@@ -25,3 +25,14 @@ print(c)
 
 
 # name(mname="Buchanan", lname="Barnes", fname="James")
+
+
+# def complexity(a ,b):
+#  print("the complexity of this function is", (a*b)/12.09)
+
+# complexity(21.8872,312)
+
+def name (fname , mname , lname):
+    print("Hi , My Full name is" , fname , mname , lname )
+  
+name(mname="ram" , fname="naresh" , lname="sharma")
