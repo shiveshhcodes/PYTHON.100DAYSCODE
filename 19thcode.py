d5938fa140e53700ac781a6c3dad2a94f415f1e6
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-04-29 17:33:53 +0530

    Create 19thcode.py

---
+++ 19thcode.py
@@ -0,0 +1,17 @@
+# today we will learn what are tupels and how to use tuples in python!! so let's start this.
+
+tuple=(1,21,34,32,445)
+print(type(tuple) , tuple)
+print(len(tuple))
+print(tuple[2])
+print(tuple[3])
+print(tuple)
+
+# a=int(input("Enter and check the number "))
+# if a in tuple:
+#  print ("yes , this number is in tuple")
+# else:
+#  print("this number does not exist")
+
+tuple2 = tuple[1:5]
+print(tuple2)
