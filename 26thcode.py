eae870e54c052d010518d35134c40c4a58e9baac
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-05 01:02:48 +0530

    Create 26thcode.py

---
+++ 26thcode.py
@@ -0,0 +1,14 @@
+# today we are gonna learn sets in python!!
+
+shivesh = {25 , "june" , 2002 , "wow" , "$$$" , 20/5 }
+print(shivesh)
+
+shivesh2 = {} # this is gonna be class of dictionary. 
+print(type(shivesh2))
+shivesh3 = set() 
+print(type(shivesh3)) # this is gonna be empty set in set of pythons
+for i in shivesh: 
+ print (i)
+raja = {12, "sidhi" , "rama"}
+print (raja)
+ 
