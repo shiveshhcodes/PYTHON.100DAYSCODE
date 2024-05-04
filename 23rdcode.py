ab6286a9cabc476a06b30d3158593e2bf1aff048
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-04 15:14:01 +0530

    Create 23rdcode.py

---
+++ 23rdcode.py
@@ -0,0 +1,16 @@
+# here we will gonna use some fun from F strings , so lets do it.
+
+name = "Shivesh"
+val = "Geeks"
+print(f"Hi , My name is {name} , I use {val}for{val} for learning coding.")
+
+"\n now we will do some fun stuffs by {f strings} using different values , lets do this.."
+val1 = int(input("Enter a value: "))
+val2 = int(input("Enter a value Again: "))
+
+"\n now let's do some fun things - "
+division = val1/val2
+print(f"Division of Value You Entered" , {val1} , "and" , {val2} , "is" , division)
+
+# ngl but this is the most useless code i have ever written on this 
+# macbook air 15 inch M2 model. 
