c76f0c59b8eb68b0f535b280fd56428c68f12e92
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-06 01:39:52 +0530

    Create 30thcode.py

---
+++ 30thcode.py
@@ -0,0 +1,48 @@
+# today we are gonna learn how to use
+# else in while loop , before we knew that it is only used in for loop but
+# today we are gonna learn , how to do it for while loop
+
+# i = 0
+# while i<139:
+#  print(i)
+#  i = i + (4) 
+#  if i==128:
+#   break
+ 
+# else:
+#     print("this loop is ended here")
+    
+# for boss in range(10):
+#     i = i+1
+#     boss = i * 2
+#     print("2 X" , i+1 , "is equals " , boss)
+# else:
+#     print("none")
+ 
+for i in range(1 , 11):  
+    boss = 13 * i
+    print("13 X", i , "is equals to", boss) 
+else:
+    print("Loop completed without interruption")
+
+# i = 1
+# while i<5:
+#     print(i)
+#     i = i+i
+# else:
+#     print("the loop is broken")
+    
+# for i in range(15):
+#  if i == 0:
+#     s = i * (i)
+#     print("the new values of" , i  , "using", '''i X (i+1)''' , "is" , s )
+#  else:
+#     s = i * (i+1)
+#     print("the new values of" , i + 1 , "using", '''i X (i+1)''' , "is" , s )
+    
+ 
+# for i in range(15): 
+#    s = i * (i + 1)  
+#    print("The value for", i , "using i * (i + 1)  is", s) 
+# else : 
+#     print("thank you")
