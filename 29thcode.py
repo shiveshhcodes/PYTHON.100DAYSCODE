71964bc7788e05c0d287a978c84b12d22d4853f5
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-06 16:07:30 +0530

    Update 29thcode.py

--- 29thcode.py
+++ 29thcode.py
@@ -1,17 +1,17 @@
 # today we are gonna learn some dictionary methods!!
 # lets do this
 
-S1  = {'shivesh': 100 , 'isha': 90 , 'ginni':-10 , 'nimki':70} 
+S1  = {'shivesh': 100 , 'isha': 90 , 'sharma':-10 , 'nimki':70} 
 # this is section 1 students marks
 
-S2 = {'lakshy': 50 , 'shreyas': 40 , 'prince':30}
+S2 = {'lakshy': 50 , 'shreyas': 40 , 'prince':30 , 'shukla':40}
 # this is section 2 students marks
 
 S1.update(S2)
 print(S1)
 S1.popitem()
 print(S1)
-S1.pop('ginni')
+S1.pop('sharma')
 print(S1)
 S1.clear()
 print(S1)
