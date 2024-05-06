0b7ad1e10160b0de036270a361091769caa94552
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-07 00:09:45 +0530

    Create 34thcode.py

---
+++ 34thcode.py
@@ -0,0 +1,20 @@
+# # CODE WITH HARRY EXCERSISE!!!!!!
+
+user_input=input("enter a value between 10 and 100: - ")
+
+if user_input.lower()==("quit"):
+    print("Thank you for using our program")
+else: 
+    try:
+        a = int(user_input)
+        if a<10 or a>100:
+         raise ValueError ("Entered value is not in range")
+        else:
+            print("You entered" , a , "is that true?")
+    except ValueError as ve:
+        print("Error" , ve)
+    finally:
+     ("Thank you for using our programmmmmmmme!!!!")
+        
+# today we are gonna learn some short hand if-else statements!!!!
+
