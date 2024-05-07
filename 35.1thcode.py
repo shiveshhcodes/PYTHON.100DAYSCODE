1b7fc1b023f3f6249fe04baea3b443a8440b37da
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-08 00:05:36 +0530

    Create 35.1thcode.py

---
+++ 35.1thcode.py
@@ -0,0 +1,7 @@
+# today we are gonna learn how to use if else statements in one line.
+
+a = int(input("ENTER A VALUE : "))
+b = int(input("ENTER A VALUE AGAIN : "))
+
+
+print("A wins") if a>b else print("=") if a==b else print("B wins")
