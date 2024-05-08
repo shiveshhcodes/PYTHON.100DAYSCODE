8cb03c388b2c7f9d5cdee723ed086096869efc75
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-08 18:20:29 +0530

    Create 37thcode.py

---
+++ 37thcode.py
@@ -0,0 +1,34 @@
+# lets discover some modules in python!!
+
+# while True: 
+#  a = int(input("enter a value to get its Square Root value: "))
+ 
+#  index = 0
+#  for i in range (a):
+#   i -=1
+#   from math import sqrt , pi
+#   result = (sqrt(a)) * pi * i
+#   print("the square root of" , a , "X 3.14 is - " , result)
+
+# while True:
+#     from math import sqrt, pi
+#     a = int(input("Enter a positive integer value: "))
+#     for i in range(a - 1, -1, -1):
+#         result = (sqrt(a)) * pi * i
+#         print(f"The square root of {a} multiplied by 3.14 and {i} is {result:.2f}")
+
+
+# while True:
+#     from math import sqrt as s, pi as s1
+#     a = int(input("Enter a positive integer value: "))
+#     for i in range(a - 1, -1, -1):
+#         result = (s(a)) * s1 * i
+#         print(f"The square root of {a} multiplied by 3.14 and {i} is {result:.2f}")
+
+
+# import math
+# print(dir(math))
+
+import math as m 
+result = m.sqrt(6767)
+print(f"{result:.2f}")
