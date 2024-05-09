167fb7e7e30a53525f995e910c00090c9688fab6
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-09 22:34:04 +0530

    Create 40thcode.py

---
+++ 40thcode.py
@@ -0,0 +1,7 @@
+import os
+if not os.path.exists("100dayscodetrial"):
+    os.mkdir("100dayscodetrial")
+    
+for i in range (1 , 101):
+    with open(f"100dayscodetrial/{i+1}.py" , "w") as tested:
+        pass
