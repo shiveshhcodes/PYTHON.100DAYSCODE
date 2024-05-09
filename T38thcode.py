89b8961165114e51075f11274d7b3f503a5e6c40
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-09 22:33:28 +0530

    Create T38thcode.py

---
+++ T38thcode.py
@@ -0,0 +1,17 @@
+# today we are gonna understand if "__name__ == "__main__" in Python
+# this file is gonna use in 39thcode.py
+def multiplication (a,b):
+        multiply = a*b
+        print("mutilplication of" ,a , "and" , b , "=" ,multiply)
+        
+# if __name__ == "__main__":
+#     a = int(input("a = "))
+#     b = int(input("b = "))
+   
+#     multiplication(a,b)
+
+if __name__ == "__main__":
+    a = 12
+    b = 12
+   
+    multiplication(a,b)
