351dededa08b6fb82984a09c2fe18999a80388bf
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-11 03:08:43 +0530

    Create 47thcode.py

---
+++ 47thcode.py
@@ -0,0 +1,42 @@
+# today we are gonna learn MAP , FILTER AND REDUCE IN PYTHON!!
+
+# MAP
+# def square (a):
+#     return a*a
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+
+# newl=[]
+# for item in l:
+#  newl.append(square(item))
+# print(newl)
+
+# newl = list(map(square , l))
+# print(newl)
+
+
+# FILTER
+
+# def maximum(a):
+#     return a>4
+
+# newll = list(filter(maximum , l))
+# # newll = list(filter(lambda a: a>6 , l))
+# print(newll)
+
+
+# REDUCE 
+
+# from functools import reduce
+
+# numbers = [1,2,3,4]
+
+# multiplication = reduce(lambda x,y: x/y , numbers)
+# print("{:.5f}".format(multiplication))
+ 
+ 
+from functools import reduce
+
+num = [1,2,11.1,4.8,5.2,6.4]
+multiply = reduce(lambda x,y: x*y*y , num)
+print("{:.3f}".format(multiply))
