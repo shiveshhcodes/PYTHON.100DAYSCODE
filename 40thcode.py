b9eef155a29b9d9eec6bf78292fb458be6004832
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-11 03:08:34 +0530

    Update 40thcode.py

--- 40thcode.py
+++ 40thcode.py
@@ -2,6 +2,6 @@ import os
 if not os.path.exists("100dayscodetrial"):
     os.mkdir("100dayscodetrial")
     
-for i in range (1 , 101):
+for i in range (1 , 3):
     with open(f"100dayscodetrial/{i+1}.py" , "w") as tested:
         pass
