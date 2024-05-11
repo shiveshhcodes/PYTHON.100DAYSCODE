a43df6039cd2c74caf99997d399d6b1b77522ab4
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-12 03:07:08 +0530

    Create 48thcode.py

---
+++ 48thcode.py
@@ -0,0 +1,21 @@
+# DAY 28 
+
+# a is b and == difference
+
+
+a = {21 , 21 , 21}
+b = [21 , 21 , 21]
+
+print(a is b)
+print(a == b)
+
+a = 21 , 21 , 21
+b = 21 , 21 , 21
+
+print(a is b)
+print(a == b)
+a = [ 21, 21 , 21]
+b = [21 , 21 ,21]
+
+print(a is b)
+print(a == b)
