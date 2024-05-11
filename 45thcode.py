de971e9d9744a14a36b906e52d72e6e7acf88485
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-11 17:14:31 +0530

    Update 45thcode.py

--- 45thcode.py
+++ 45thcode.py
@@ -1,4 +1,4 @@
-# lets discover and learn seek() and learn() and truncate() functions in python!! (day 27)
+# lets discover and learn seek() and tell() and truncate() functions in python!! (day 27)
 
 # LETS WRITE THE CODE HERE
 
@@ -8,7 +8,7 @@
 #  LETS RUN SEEK HERE AND SKIP THE CODE TO AFTER 7 CHARACTERS!!!
 
 # f = open('45thcode.txt' , 'r')
-# f.seek(7)
+# f.seek(2)
 
 # result = f.read()
 # print(result)
