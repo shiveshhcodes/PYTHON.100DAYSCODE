cf2c64a4c0776dca68fa47cb8014b8424cc3250a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-13 02:23:06 +0530

    Update 9thcode.py

--- 9thcode.py
+++ 9thcode.py
@@ -1,26 +1,33 @@
-#let's discover various strings of python today.
+# #let's discover various strings of python today.
 
-string1 = " ShIVVVesH "
-print(string1.upper())
-print(string1.lower())
+# string1 = " ShIVVVesH "
+# # print(string1.upper())
+# # print(string1.lower())
 
-string2 = " ShIVEEEEsHHH "
-print(string2.strip)
+# print(string1.lower())
+# print(string1.upper())
 
-string3 = "Hello Shivesh !!!"
-print(string3.rstrip("!"))
+# string2 = " ---ShIVEEEEsHHH--- "
+# print(string2.strip("-"))
 
-string4 = "Hello Shiveshhh"
-print(string4.replace("Hello" , "Hi"))
+# string3 = "Hello Shivesh !!!"
+# print(string2.rstrip("-"))
 
-string5 = "Namaste Sirji"
-print (string5.split(" "))
+# string4 = "Hello Shiveshhh"
+# # print(string4.replace("Hello" , "Hi"))
+# print(string4.replace("Shiveshhh" , "shivesh25"))
 
-string6 = "JAI SHRI RAM"
-print(string6.capitalize())
+# string5 = "Namaste Sirji"
+# # print (string5.split(" "))
+# print (string5.split())
 
-string7 = "SHIVESH RICHHARIYA"
-print(string7.center(89 , ":"))
+# string6 = "JAI SHRI RAM"
+# print(string6.capitalize())
 
-string8 = "shibusokoks"
+# string7 = "SHIVESH RICHHARIYA"
+# # print(string7.center(89 , ":"))
+# print(string7.center(98 , "-"))
+
+string8 = "shibusokoksjsdbjsdhubsnslifnlnclsnssnfirls"
+# print(string8.count("s"))
 print(string8.count("s"))
