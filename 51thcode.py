6196bb6f1f2099e95074076561021dc806fd12dd
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-13 02:22:59 +0530

    Create 51thcode.py

---
+++ 51thcode.py
@@ -0,0 +1,50 @@
+# # DAY 29
+
+# # today we are gonna learn of constructors and its types!!
+
+# class Humans:
+#     def __init__(self , name , gender , skin):
+#         self.name = name
+#         self.gender = gender
+#         self.skin = skin
+        
+#     def info(self):
+#         print(f"{self.name} is {self.gender} and {self.skin} in colour")
+        
+    
+# a = Humans("Shivesh" , "male" , "brown")
+# b = Humans("Shakshi" , "female" , "white")
+# c = Humans("Shivangi" , "female" , "brown")
+# a.info()
+# b.info()
+# c.info()
+
+
+# class cars:
+#     def __init__ (self , name , engine):
+#         self.name = name
+#         self.engine = engine
+        
+#     def info(cars):
+#         print(f"the car name is {cars.name} and it has {cars.engine} in it")
+        
+        
+# RR = cars("RR" , "v8")
+# BMW = cars("BMW" , "V6")
+
+# RR.info()
+# BMW.info()
+
+# This program defines two functions to perform integer and float division
+# and prompts the user to enter two numbers to divide.
+
+
+class apple:
+    def __init__ (self , name):
+     self.name = name
+     
+    def info(apple):
+        print(f"the name of apple device is {apple.name}")
+        
+shivesh = apple("macbook 15inch")
+shivesh.info()
