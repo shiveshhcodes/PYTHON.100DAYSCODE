82d070dc4df5a48493461b19ad04f89b6d6dcaea
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-13 02:36:59 +0530

    Update 10thcode.py

--- 10thcode.py
+++ 10thcode.py
@@ -64,28 +64,28 @@
 a = input("WHAT IS YOUR NAME???? : ")
 print("\nHi", a, "I hope you are doing fantastic")
 
-b = input("\nDo You agree to all our T&C's? (yes/no) : ")
-c = input("\nDo You READ to all our T&C's? (yes/no) : ")
+b = input("\nDo You Agree & Read to all our T&C's? (yes/no) : ")
+# c = input("\nDo You READ to all our T&C's? (yes/no) : ")
 
-if c.lower() == "yes" and b.lower() == "yes":
+if b.lower() == "yes":
     print("Let's do it then")
 
     s = int(input("\nTell me your Age?? " + a + ":"))
     print("Your Age is:", s)
-    if s > 20:
-        print("You can go to college now, You are an adult ")
+    if s > 18:
+        print("You can drive any vehicle, You are an adult ")
         ss = input("\nAre you happy now?? (Yes/No): ")
         if ss.lower() == "yes":
-            print("Goodddddddds")
+            print("great to hear that")
         else:
-            print("\nBaddddd Habittttt")
-            print("GO AND STUDY")
+            print("\nthat's wrong")
+            # print("GO AND STUDY")
 
     elif s == 18:
-        print("I guess you just passed your 12th, you can go to\nApply for college next year buddy.")
+        print("I guess you just passed your 12th, you can go to\nApply for Driving License buddy.")
 
     else:
-        print("You cannot go to college now")
+        print("You cannot drive any vehicle now!! except cycle")
 else:
  print("\nThank you for checking my program.\n\nHave a Good Day", a, ":)")
 
