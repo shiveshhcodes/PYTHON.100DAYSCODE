8366786970a1951beb1f88f2ed7244f90f307e49
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-14 18:13:54 +0530

    Create 52thcode.py

---
+++ 52thcode.py
@@ -0,0 +1,84 @@
+# # today we are gonna learn and discover what decorators are in python!!
+
+# def greet(fx):
+#     def mfx(*args , **kwargs):
+#      print("GM")
+#      fx(*args , **kwargs)
+#      print("GN")
+#     return mfx
+    
+
+# @greet
+# def add(a, b):
+#    add = (a+b)
+#    print("the sum of two numbers is" , add)
+   
+# @greet
+# def diff(a, b):
+#    diff = (a-b)
+#    print("the difference of two numbers is" , diff)
+   
+# add(21,21)
+# diff(21,21)
+
+
+# def greet(fx):
+#     def mfx(*args , **kwargs):
+#         print("Good Morning")
+#         fx(*args , **kwargs)
+#         print("good Night")
+        
+#     return mfx
+
+# @greet
+# def add(a, b):
+#    add = (a+b)
+#    print("the sum of two numbers is" , add)
+   
+# @greet
+# def diff(a, b):
+#    diff = (a-b)
+#    print("the difference of two numbers is" , diff)
+   
+# add(244,21)
+# diff(221,21)
+
+
+
+# def greet(fx):
+#     def mfx(*args , **kwargs):
+#         print("Hi sir/mam")
+#         fx(*args , **kwargs)
+#         print("thank you")
+
+#     return mfx
+# @greet
+# def add(a, b):
+#    add = (a+b)
+#    print("the sum of two numbers is" , add)
+   
+# @greet
+# def diff(a, b):
+#    diff = (a-b)
+#    print("the difference of two numbers is" , diff)
+   
+# add(244,21)
+# diff(221,21)
+
+def greet (fx):
+    def mfx(*args , **kwargs):
+        print("GMGM")
+        fx(*args, **kwargs)
+        print("GNGN")
+    return mfx
+
+@greet
+def add(a,b):
+ addition = a+b
+ print ("sum of two numbers you entered is" , addition)
+ 
+
+a = int(input("Enter first value "))
+b = int(input("Enter second value "))
+result = add(a,b)
+print(result)
