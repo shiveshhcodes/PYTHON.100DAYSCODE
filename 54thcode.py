d953bfe35e642d1ba19468c2009d1ea140e4cfdb
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-15 22:57:13 +0530

    inheritence

---
+++ 54thcode.py
@@ -0,0 +1,78 @@
+# # # today we are gonna learn on inheritance in python
+
+# # class Employee:
+# #   def __init__(self, name, id):
+# #     self.name = name
+# #     self.id = id 
+
+# #   def showDetails(self):
+# #     print(f"The name of Employee: {self.id} is {self.name}")
+
+# # class Programmer(Employee):
+# #   def showLanguage(self):
+# #     print("The default langauge is Python")
+
+
+# # e1 = Employee("SanAI", 400)
+# # e1.showDetails()
+# # e2 = Programmer("Shivesh", 4100)
+# # e2.showDetails()
+# # e2.showLanguage()
+
+# # class Employee:
+# #   def __init__(self , name , ID , gender):
+# #     self.name = name
+# #     self.ID = ID
+# #     self.gender = gender
+    
+# #   def info(self):
+# #     print(f"{self.name} is a employee of our company , there ID is {self.ID} and there gender is {self.gender}")
+
+# # class HR(Employee):
+# #   def details(self):
+# #     print("Hi , i am HR of your company")
+
+# # shivesh = HR("Shivesh" , "21" , "Male")
+# # SanAI = Employee("SanAI" , "19" , "Female")
+# # shivesh.details()
+# # SanAI.info()
+
+
+# class Brosis:
+#   def __init__(self , role , name , age):
+#     self.name = name
+#     self.age = age
+#     self.role = role
+    
+#   def ShowDetails(self):
+#      print(f"{self.name} is {self.role} and there age is {self.age}")
+     
+# class Family(Brosis):
+#   def details(self):
+#     print(f"{self.name} age is {self.age}")
+     
+# shivesh = Family("brother" , "shivesh" , "21")
+# Shakshi = Brosis("sister" , "shakshi" , "29")
+
+# Shakshi.ShowDetails()
+# shivesh.details()
+
+class voters:
+  def __init__(self , name , age):
+    self.name = name
+    self.age = age
+    
+  def details(self):
+    print(f"{self.name} has VoterID and his age is {self.age}")
+    
+class more(voters):
+  def moredetails(self):
+    print(f"INDIA IS A SECURAL COUNTRY {self.name} you got {self.age} recently")
+    
+shivesh = voters("shivesh" , "21")
+shakshi = voters("shakshi" , "29")
+shakshi = more("shakshi" , "29")
+
+shivesh.details()
+shakshi.details()
+shakshi.moredetails()
