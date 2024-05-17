74264f9d9832b01e4e03ac7ef9235947caf0f40d
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-17 23:46:10 +0530

    Create 57thcode.py

---
+++ 57thcode.py
@@ -0,0 +1,36 @@
+# class Math:
+#   def __init__(self, num):
+#     self.num = num
+
+#   def addtonum(self, n):
+#     self.num = self.num +n
+    
+#   @staticmethod
+#   def add(a, b):
+#       return a + b
+
+# # result = Math.add(1, 2)
+# # print(result) # Output: 3
+# a = Math(5)
+# print(a.num)
+# a.addtonum(6)
+# print(a.num)
+
+# print(Math.add(7, 2)) 
+
+class sum:
+  def __init__(self , num):
+   self.num = num
+   
+  def addtonum (self , n):
+   self.num = self.num + n
+    
+  @staticmethod
+  def add(a,b):
+      return (a+b)
+ 
+a = sum(21)
+print(a.num)
+
+a.addtonum(21)
+print(a.num)
