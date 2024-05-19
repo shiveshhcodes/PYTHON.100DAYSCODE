5c2f91d6ad48af591df86f19bb31a4ea5b5f603a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-19 14:55:28 +0530

    Update 56thcode.py

--- 56thcode.py
+++ 56thcode.py
@@ -1,23 +1,51 @@
-class library:
-    def __init__(self):
-        self.no_of_books = 0
-        self.books =[]
-    def addbook(self, book):
-        self.books.append(book)
-        self.no_of_books = len(self.books)
+# class library:
+#     def __init__(self):
+#         self.no_of_books = 0
+#         self.books =[]
+#     def addbook(self, book):
+#         self.books.append(book)
+#         self.no_of_books = len(self.books)
     
-    def result(self):
-        print(f"\nlibrary hass {self.no_of_books} books and those books are - \n")
+#     def result(self):
+#         print(f"\nlibrary hass {self.no_of_books} books and those books are - \n")
         
-        for book in self.books:
-         print(book)
+#         for book in self.books:
+#          print(book)
     
-number = library()
-number.addbook("atomic habits")
-number.addbook("Healthy habits")
-number.addbook("Unhealthy habits")
+# number = library()
+# number.addbook("atomic habits")
+# number.addbook("Healthy habits")
+# number.addbook("Unhealthy habits")
+# number.addbook("Unhealthy habits")
+# number.addbook("Unhealthy habits")
+# number.addbook("Unhealthy habits")
+# number.addbook("Unhealthy habits")
+# number.addbook("Unhealthy habits")
 
 
  
      
+# number.result()
+
+class library:
+    def __init__(self):
+        self.no_of_books = 0
+        self.books = []
+        
+    def addbook(self , book):
+        self.books.append(book)
+        self.no_of_books = len(self.books)
+        
+    def result(self):
+        print(f"The Library has {self.no_of_books} books and those are- ")
+        
+        for book in self.books:
+         print(book)
+
+number = library()
+number.addbook("arm shifter")
+number.addbook("leg shifter")
+number.addbook("head shifter")
+
+
 number.result()
