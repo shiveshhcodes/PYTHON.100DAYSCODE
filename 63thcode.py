10dc3a40ef6beb5b3581101a7e2c51e3e75b0f8f
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-22 20:16:29 +0530

    Update 63thcode.py

--- 63thcode.py
+++ 63thcode.py
@@ -1,7 +1,7 @@
 from a import college
 
-b = college("shiveshhhhh" ,  21)
+b = college("IPS", 25)
 print(b)
 print(b.name)
-print(b.number)
-print("the length of name is" , len(b))
+# print(b.number)
+# print("the length of name is" , len(b))
