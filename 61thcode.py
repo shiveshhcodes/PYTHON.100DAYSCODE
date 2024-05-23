d8d01de74534c0e05df694d467782a6c3605f57b
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-23 23:35:11 +0530

    Update 61thcode.py

--- 61thcode.py
+++ 61thcode.py
@@ -1,10 +1,10 @@
 class person:
-  def __init__(self , name , age):
-    self.name = name
+  def __init__(self , naam , age):
+    self.naam = naam
     self.age = int(age)
     
   def show(self):
-    print(f"Hi My name is {self.name} age is {self.age}")
+    print(f"Hi My naam is {self.naam} age is {self.age}")
   
 shivesh = person("shivesh" , 21)
 shivesh.show()
