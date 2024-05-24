55662886da23274df3d58babd8ea9639d9fc9f17
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-24 23:51:26 +0530

    Update 26thcode.py

--- 26thcode.py
+++ 26thcode.py
@@ -9,6 +9,6 @@ shivesh3 = set()
 print(type(shivesh3)) # this is gonna be empty set in set of pythons
 for i in shivesh: 
  print (i)
-raja = {12, "sidhi" , "rama"}
+raja = {12, "sidhi" , "ram"}
 print (raja)
  
