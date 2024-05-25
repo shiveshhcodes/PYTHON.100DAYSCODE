78770b44a9047a63cd33a2889bab482d87711574
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-25 19:14:22 +0530

    Create 71thcode.py

---
+++ 71thcode.py
@@ -0,0 +1,29 @@
+# this is gonna be example of Hierarchical Inheritence
+
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def show_details(self):
+        print("Name:", self.name)
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name)
+        self.breed = breed
+
+    def show_details(self):
+        Animal.show_details(self)
+        print("Species: Dog")
+        print("Breed:", self.breed)
+
+class Cat(Animal):
+    def __init__(self, name, color):
+        Animal.__init__(self, name)
+        self.color = color
+
+    def show_details(self):
+        Animal.show_details(self)
+        print("Species: Cat")
+        print("Color:", self.color)
