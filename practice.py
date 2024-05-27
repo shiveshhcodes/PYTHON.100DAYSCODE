2710ba364cfdc503bc987e7786f2849e92d39abc
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 02:54:01 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,20 @@
+# print(fruits)
+# new_str = ' , '.join(fruits)
+# print(new_str)
+# print(' , '.join(fruits))
+# fruits = ['apple' , 'banana' , 'orange']
+# text_split = text.split()
+# print(text_split)
+# text_strip = text.strip()
+# print(text_strip)
+# new_text = text.replace('world' , 'python')
+# print(new_text)
+# text_upper = text.upper()
+# text_lower = text.lower()
+# print(text_lower)
+# print(text_upper)
+
+
+text = 'hi Hello world Welcome to new world'
+count = ','.join(text)
+print(count)
