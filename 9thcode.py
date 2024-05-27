ce6d8c018c73009bcfe6d03c45430ac9a5babc15
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 02:53:59 +0530

    Update 9thcode.py

--- 9thcode.py
+++ 9thcode.py
@@ -7,27 +7,37 @@
 # print(string1.lower())
 # print(string1.upper())
 
-# string2 = " ---ShIVEEEEsHHH--- "
-# print(string2.strip("-"))
+string2 = " ShIVEEEEsHHH "
+print(string2.strip("-"))
 
 # string3 = "Hello Shivesh !!!"
-# print(string2.rstrip("-"))
+print(string2.rstrip("-"))
 
 # string4 = "Hello Shiveshhh"
 # # print(string4.replace("Hello" , "Hi"))
 # print(string4.replace("Shiveshhh" , "shivesh25"))
 
-# string5 = "Namaste Sirji"
-# # print (string5.split(" "))
-# print (string5.split())
+string5 = "Namaste Sirji"
+# print(','.join(string5))
+# print (string5.split(",."))
+print (string5.split())
 
-# string6 = "JAI SHRI RAM"
-# print(string6.capitalize())
+string6 = "JAI SHRI RAM"
+print(string6.capitalize())
 
-# string7 = "SHIVESH RICHHARIYA"
-# # print(string7.center(89 , ":"))
-# print(string7.center(98 , "-"))
+string7 = "SHIVESH RICHHARIYA"
+# print(string7.center(89 , ":"))
+print(string7.center(38 , ","))
+
+# string8 = "shibusokoksjsdbjsdhubsnslifnlnclsnssnfirls"
+# print(string8.count("s"))
+from collections import Counter
 
 string8 = "shibusokoksjsdbjsdhubsnslifnlnclsnssnfirls"
-# print(string8.count("s"))
-print(string8.count("l"))
+letter_counts = Counter(string8)
+
+print(letter_counts)
+print(len(string8))
+
+
+# print(string8.count("l"))
