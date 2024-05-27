89b607087d1c5ddaaf7e0fce832882a1b7a6179b
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 00:06:35 +0530

    Update 72thcode.py

--- 72thcode.py
+++ 72thcode.py
@@ -19,13 +19,16 @@ import time
 # # print(t1)
 # print(t2)
 
-def usingI():
-    for i in range(101):
-      print(i)
-init = time.time()
-usingI()
-time.sleep(1.5)
-print("the total time + 1.5 second delay is = " , time.time() - init)
+# def usingI():
+#     for i in range(5):
+#       print(i)
+# start = time.time()
+
+
+
+# usingI()
+# time.sleep(1.5)
+# print("the total time + 1.5 second delay is = " , time.time() - start)
   
   
 # # print(4)
@@ -39,12 +42,12 @@ print("the total time + 1.5 second delay is = " , time.time() - init)
 
 # import time
 
-# end_time = time.time() + 200  # Calculate the end time, 20 seconds from now
+end_time = time.time() + 20 # Calculate the end time, 20 seconds from now
 
-# while time.time() < end_time:
-#     t = time.localtime()
-#     current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
-#     print(current_time)
-#     time.sleep(5)  # Wait for 1 second before printing the time again
+while time.time() < end_time:
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
+    time.sleep(1)  # Wait for 1 second before printing the time again
 
 
