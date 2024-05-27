8f748657a94a3f7c8fa9aa7b1e9d7d8818b641c3
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 00:06:40 +0530

    Update 8thcode.py

--- 8thcode.py
+++ 8thcode.py
@@ -1,8 +1,14 @@
-name = "shivesh richhariyaaaa"
+name = "shivesh richhariya"
 namelen = len(name)
 print (namelen) 
 
-print (name[0:3]) #this includes 0 but not 4
-print (name[0:5]) #this includes 0 but not 8
-print (name[3:10]) #this includes 3 but not 10
-print (name[4:7]) 
+# print (name[0:3]) #this includes 0 but not 4
+# print (name[0:5]) #this includes 0 but not 8
+# print (name[3:10]) #this includes 3 but not 10
+# print (name[4:7]) 
+
+# name_parts = name.split()
+# initails = for part in name_parts
+initails_str = ':'.join(name)
+
+print(initails_str)
