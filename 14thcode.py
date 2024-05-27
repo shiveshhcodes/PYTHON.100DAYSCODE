e896cb7983964a0dafa344e39099db6b792509a9
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 03:36:43 +0530

    Update 14thcode.py

--- 14thcode.py
+++ 14thcode.py
@@ -23,14 +23,38 @@
 #     if(i % 100 == 15):
 #         break
 
-while True:
-    age = input("Enter your age: ")
-    if age.isdigit():
-        age = int(age)
-        if age >= 0:
-            print(f"Your age is {age}.")
-            break
-        else:
-            print("Age cannot be negative.")
-    else:
-        print("Invalid input. Please enter a number.")
+# while True:
+#     age = input("Enter your age: ")
+#     if age.isdigit():
+#         age = int(age)
+#         if age >= 0:
+#             print(f"Your age is {age}.")
+#             break
+#         else:
+#             print("Age cannot be negative.")
+#     else:
+#         print("Invalid input. Please enter a number.")
+
+# import time
+
+# countdown = 100
+# while countdown > 0:
+#     print(f"{countdown}")
+#     countdown -= 2
+#     time.sleep(0.1)  # Delay of 1 second
+# print("Time's up!")
+
+
+# for i in range(1 , 21):
+#     print(f" 19 times {i} is { 19 * i}")
+
+import time
+
+countdown = 1000000000
+while countdown > 0:
+    print(f"Remaining time is : {countdown} seconds")
+    countdown -= 10000
+    time.sleep(0.0000001)
+    # time.strptime("%H:%M:%S")
+    
+print("time's UP")
