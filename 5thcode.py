d2d4898028d56b4d87beb1700a4923e7125a3786
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 00:06:30 +0530

    Update 5thcode.py

--- 5thcode.py
+++ 5thcode.py
@@ -1,13 +1,26 @@
-#here we learned about , what is typecasting and how does it works for all.
+# #here we learned about , what is typecasting and how does it works for all.
 
-a = 999
-b = 22323
+# a = 999
+# b = 22323
 
-print("the Multiplication of" , a , "and" , b , "is equal to" , a*b)
+# print("the Multiplication of" , a , "and" , b , "is equal to" , a*b)
 
-# now let use these numbers as strings and not as integers
+# # now let use these numbers as strings and not as integers
 
-a = "1298"
-b = "2.92" #here value of b is in float
+# a = "1298"
+# b = "2.92" #here value of b is in float
 
-print ("the sum of" , int(a) , "and" , float(b), "is" ,int(a)+float(b))
+# print ("the sum of" , int(a) , "and" , float(b), "is" ,int(a)+float(b))
+
+# Concatenation
+full_name = 'Alice' + ' ' + 'Smith'
+
+# Slicing
+first_letter = full_name[0]  # 'A'
+
+# Methods
+uppercase_name = full_name.upper()  # 'ALICE SMITH'
+
+print(full_name)
+print(first_letter)
+print(uppercase_name)
