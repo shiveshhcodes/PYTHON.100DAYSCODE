2dfa018eb9135196fc247ffd5abfa3c9b0f9a595
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 19:47:21 +0530

    Update 17thcode.py

--- 17thcode.py
+++ 17thcode.py
@@ -1,6 +1,13 @@
 # here we will learn about lists.
 
 marks = [0,12,23,34,45,56,67,78,89,121,156]
+marks.append(2121)
+marks.remove(0)
+marks.insert(0 , 9)
+marks.sort()
+marks.reverse()
+# marks.len(marks)
+print(marks)
 # print(marks)
 # print(type(marks))
 # print(marks[0])
@@ -20,9 +27,9 @@ marks = [0,12,23,34,45,56,67,78,89,121,156]
 # print (marks[1:-1])
 # print (marks[1:4:2])
 
-lst = [i*i for i in range(234) if i%55==0]
-print(lst)
+marks = [i*i for i in marks if i%5==0]
+print(marks)
 
-lst = [i+i for i in range(5) if i%2==0]
+# lst = [i+i for i in range(5) if i%2==0]
 
 
