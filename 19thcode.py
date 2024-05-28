8f090a9975079e901c544378d831059c105a5abe
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 19:47:26 +0530

    Update 19thcode.py

--- 19thcode.py
+++ 19thcode.py
@@ -1,17 +1,26 @@
-# today we will learn what are tupels and how to use tuples in python!! so let's start this.
+# # today we will learn what are tupels and how to use tuples in python!! so let's start this.
 
-tuple=(1,21,34,32,445)
-print(type(tuple) , tuple)
-print(len(tuple))
-print(tuple[6])
-print(tuple[3])
-print(tuple)
+# tuple=(1,21,34,32,445)
+# print(type(tuple) , tuple)
+# print(len(tuple))
+# # print(tuple[6])
+# print(tuple[3])
+# # print(tuple)
 
-# a=int(input("Enter and check the number "))
-# if a in tuple:
-#  print ("yes , this number is in tuple")
-# else:
-#  print("this number does not exist")
+# # a=int(input("Enter and check the number "))
+# # if a in tuple:
+# #  print ("yes , this number is in tuple")
+# # else:
+# #  print("this number does not exist")
 
-tuple2 = tuple[1:5]
-print(tuple2 , tuple2.count(21))
+# # tuple2 = tuple[1:5]
+# # print(tuple2 , tuple2.count(21))
+
+# # fruit_tuple = ('apple', 'banana', 'cherry')
+# # fruit_tuple[1] = 'mango'  # This will raise a TypeError
+
+person = ('John', 30, 'Engineer')
+name, age, profession = person
+print("name =" , name)      # Output: 'John'
+print(age)       # Output: 30
+print(profession)  # Output: 'Engineer'
