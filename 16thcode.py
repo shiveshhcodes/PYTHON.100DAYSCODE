70ae9f56989f1f81e498a8206cb457dd855132b4
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 19:47:19 +0530

    Update 16thcode.py

--- 16thcode.py
+++ 16thcode.py
@@ -20,19 +20,19 @@
 
 
 # def name(**name):
-#   # print(type(name))
+#   print(type(name))
 #   print("Hello,", name["fname"], name["mname"], name["lname"])
 
 
 # name(mname="Buchanan", lname="Barnes", fname="James")
 
 
-# def complexity(a ,b):
-#  print("the complexity of this function is", (a*b)/12.09)
+def complexity(a ,b):
+ print("the complexity of this function is", (a*b)/12.09)
 
-# complexity(21.8872,312)
+complexity(21,312)
 
-def name (fname , mname , lname):
-    print("Hi , My Full name is" , fname , mname , lname )
+# def name (fname , mname , lname):
+#     print("Hi , My Full name is" , fname , mname , lname )
   
-name(mname="rama" , fname="nareshing" , lname="sharmaaaa")
+# name(mname="rama" , fname="nareshing" , lname="sharmaaaa")
