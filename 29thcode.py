4c97afa3e7077eb76ad537da1ed954840b6c0fe7
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-29 04:05:33 +0530

    Update 29thcode.py

--- 29thcode.py
+++ 29thcode.py
@@ -4,7 +4,7 @@
 S1  = {'shivesh': 100 , 'isha': 90 , 'sharma':-10 , 'nimki':70} 
 # this is section 1 students marks
 
-S2 = {'lakshy': 50 , 'shreyas': 40 , 'prince':30 , 'shukla':40}
+S2 = {'lakshy': 00 , 'shreyas': 40 , 'prince':30 , 'shukla':40}
 # this is section 2 students marks
 
 S1.update(S2)
