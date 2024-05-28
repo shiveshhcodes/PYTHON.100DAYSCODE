2b2cb1d4b65f08f7e26e61ac5f96f45344ec2e26
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-29 04:05:17 +0530

    Update 20thcode.py

--- 20thcode.py
+++ 20thcode.py
@@ -5,15 +5,39 @@
 # temp = list(countries)
 # temp.append("Russia")       #add item 
 # temp.pop(3)                 #remove item
-# temp[2] = "Finland"         #change item
+# temp[0] = "Finland"         #change item
 # countries = tuple(temp)
 # print(countries)
 
-Studentsname = ("shivesh" , "rajjj" , "akshay" , "kapil" , "prince")
+Studentsname = ("shivesh" , "raj" , "akshay" , "kapil" , "prince")
 temp = list(Studentsname)
 temp.append("akhit , rahgir")
 temp.pop(3)
 temp[4] = "ashwin"
 Studentsname = tuple(temp)
-print(Studentsname)
 print(Studentsname.index("raj"))
+print(Studentsname)
+
+# from typing import Tuple
+
+# def get_person_info() -> Tuple[str, int, float]:
+#     name = "John Doe"
+#     age = 30
+#     height = 1.75
+#     return name, age, height
+
+# person_info = get_person_info()
+
+# from typing import Tuple
+
+# def get_person_info():
+#     name = input("Enter your name: ")
+#     age = int(input("Enter your age: "))
+#     height = float(input("Enter your height in meters: "))
+#     return name, age, height
+
+# person_info = get_person_info()
+# print(person_info)  # Output will depend on user input
+
+# name, age, height = person_info
+# print(f"Name: {name}, Age: {age}, Height: {height}")
