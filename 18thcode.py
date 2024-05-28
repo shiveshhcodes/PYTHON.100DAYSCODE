3134d2330dc3a9ef0b8ab806b76c0e69c05a2e95
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-28 19:47:23 +0530

    Update 18thcode.py

--- 18thcode.py
+++ 18thcode.py
@@ -2,11 +2,11 @@
 
 l=[12,23,34,45,56,67,78,89,90,110]
 d=[1212,1122]
-l.append(21)
-l.sort()
-# l.sort(reverse=True)
-print(l.count(67))
-print(l.index(34))
-l.insert(3,122)
+# l.append(21)
+# l.sort()
+# # l.sort(reverse=True)
+# print(l.count(67))
+# print(l.index(34))
+# l.insert(3,122)
 l.extend(d)
 print(l)
