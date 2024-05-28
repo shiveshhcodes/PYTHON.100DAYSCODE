ed3f92cdb527885bb5f07d735648e51da0d5fb59
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-29 04:05:19 +0530

    Update 21thcode.py

--- 21thcode.py
+++ 21thcode.py
@@ -1,23 +1,23 @@
-# import time
-# t = time.strftime('%H:%M:%S')
-# Hour = int(time.strftime('%H'))
-# if (Hour>=0 and Hour<12):
-#  print("Good Morning Shivesh")
-# elif (Hour>12 and Hour<17):
-#  print("Good Evening Shivesh")
-# # elif (Hour>=17 and Hour<0):
-# else
-#  print ("Good Night Shivesh")
-
-
 import time
+t = time.strftime('%H:%M:%S')
+Hour = int(time.strftime('%H'))
+if (Hour>=0 and Hour<12):
+ print("Good Morning Shivesh")
+elif (Hour>12 and Hour<17):
+ print("Good Evening Shivesh")
+# elif (Hour>=17 and Hour<0):
+else:
+ print ("Good Night Shivesh")
 
-t = time.strftime('%H:%M:%S')  
-Hour = int(time.strftime('%H'))  
 
-if 0 <= Hour < 12:  
-    print("Good Morning Shiveshhh")
-elif 12 <= Hour < 17:  
-    print("Good Afternoon Shiveshhh")
-else:  
-    print("Good Evening Shivesh")
+# import time
+
+# t = time.strftime('%H:%M:%S')  
+# Hour = int(time.strftime('%H'))  
+
+# if 0 <= Hour < 12:  
+#     print("Good Morning Shiveshhh")
+# elif 12 <= Hour < 17:  
+#     print("Good Afternoon Shiveshhh")
+# else:  
+#     print("Good Evening Shivesh")
