e09d445d3fd309b6d04b2bd9c5afa21668a162b8
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-30 01:25:25 +0530

    Update 40thcode.py

--- 40thcode.py
+++ 40thcode.py
@@ -1,7 +1,20 @@
 import os
-if not os.path.exists("100dayscodetrial"):
-    os.mkdir("100dayscodetrial")
+# if not os.path.exists("100dayscodetrial"):
+#     os.mkdir("100dayscodetrial")
     
-for i in range (1 , 3):
-    with open(f"100dayscodetrial/{i+1}.py" , "w") as tested:
-        pass
+# for i in range (1 , 3):
+#     with open(f"100dayscodetrial/{i+1}.py" , "w") as tested:
+#         pass
+
+cwd = os.getcwd()
+# print(f"Current working directory: {cwd}") 
+
+newdir = os.listdir(cwd)
+# newdir_ls = list(newdir)
+# print(newdir_ls)
+# index = 0
+# print(f"current working directory is {cwd}")
+# newdir_ls.sort()
+# newdir_ls.reverse()
+for index , item in enumerate(newdir):
+    print(f"{index}. {item}")
