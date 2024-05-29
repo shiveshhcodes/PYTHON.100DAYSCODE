d9a2975d51b55d5db343eac950fba80985688900
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-30 01:25:07 +0530

    Update 31thcode.py

--- 31thcode.py
+++ 31thcode.py
@@ -1,19 +1,24 @@
 # today we are gonna learn , exception handling.
 
 # lets begin
-a = int(input("Enter a value to get Table of it - "))
-print(f"The Multiplication Table of {a} is :")
  
 # table = int(a) * i 
 # print(f"The Multiplication table of {int(a)} X {i} is" , table)
-try:  
+while True:
+ try:  
+    a = int(input("Enter a value to get Table of it - "))
+    print(f"The Multiplication Table of {a} is :")
+    for i in range (1,11):
+        result = int(a) * i
+        print(f"{int(a)} X {i} = " , result)
+    break
+ except ValueError:
+      print("please enter a number , not alphabets")
+  # finally:
+  #     print("See you again!!")
 
- for i in range (1,11):
-   result = int(a) * i
-   print(f"{int(a)} X {i} = " , result)
-except ValueError:
-    print("please enter a number , not alphabets")
-print("Thanks")
+
+  
 
 # try:
 #   c = int(input("enter a integer value: " ))
