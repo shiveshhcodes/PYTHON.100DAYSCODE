5f15838b689bba046c1402c8694b13fe7ed82bcd
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-30 01:25:37 +0530

    Update 44thcode.py

--- 44thcode.py
+++ 44thcode.py
@@ -1,17 +1,18 @@
-f = open('44.1thcode.txt' , 'r')
-# lines = ['11th line of code\n' , '22nd line of code\n' , '33rd line of code\n']
-# lines = ['11th line of code\n' , '22nd line of code\n' , '33rd line of code\n']
-# f.writelines(lines)
+with open('44.1thcode.txt' , 'r') as f:
+ result = f.read()
+ print(result)
+# lines = ['11th line of code\n' , '22nd line of code\n' , '33rd line of code\n', '11th line of code\n' , '22nd line of code\n' , '33rd line of code\n']
 
-while True:
-    helloo = f.readline()
-    if not helloo:
-        break
-    print(helloo)
+
+# while True:
+#     helloo = f.readline()
+#     if not helloo:
+#         break
+#     print(helloo)
     
 
 # while True:
-#     line = f.readline()
+#     line = f.read()
 #     if not line:
 #         break
 #     print(line)
