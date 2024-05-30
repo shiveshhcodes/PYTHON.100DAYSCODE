4e7dc4cdd9ae3ca85c59d708ee8054275a080541
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-30 22:48:04 +0530

    Update 53thcode.py

--- 53thcode.py
+++ 53thcode.py
@@ -1,18 +1,20 @@
 def is_leap(year):
-    leap = False
+    # leap = False
      
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
             else:
-                return False
+                return 'Its not a leap year'
         else: 
-            return True
+            return 'Its a leap year'
     else:
-        return False
+        return 'Its not a leap year'
      
         return leap
 
-year = int(input())
+year = int(input("enter a Year to check if it's a leap year or not: "))
 print(is_leap(year))
+
+
