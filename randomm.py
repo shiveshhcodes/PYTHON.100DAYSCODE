e8834f570cecf5c41f5b34240acb4512f54fd53a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-01 00:49:31 +0530

    Create randomm.py

---
+++ randomm.py
@@ -0,0 +1,21 @@
+# today we are gonna discover shutil module.
+import shutil
+# import os
+
+
+shutil.copy2("76thcode.py" , "randomm.py")
+# os.rename("randomm.py" , "shutil_model_copy_76th")
+# os.remove("random.py")
+# os.listdir("/Users/shiveshrichhariya/Desktop/GITHUB/100 DAYS OF CODE/PYTHON.100DAYSCODE")
+
+# import os
+# from pathlib import Path
+
+# # Using os.getcwd()
+# cwd = os.getcwd()
+# print(cwd)
+
+# Using pathlib.Path.cwd()
+# cwd = Path.cwd()
+# print(cwd)
+# os.remove("try.txt")
