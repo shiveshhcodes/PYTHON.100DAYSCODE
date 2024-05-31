f6d2b3a885f8bb30aa5ea59eb48a61ba737bf201
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-05-31 16:44:13 +0530

    Create 75thcode.py

---
+++ 75thcode.py
@@ -0,0 +1,14 @@
+# practiceing walrus operator - 
+
+foods = []
+while (food := input("Enter any Number: ").lower()) !="quit":
+    while True:
+       if not food.strip():
+           print("please do not leave space")
+           break
+       elif not food.strip().isdigit():
+            print("please enter a number")
+            break
+       foods.append(food)
+       break
+print(f"so here is the list of numbers you entered = {foods} ")
