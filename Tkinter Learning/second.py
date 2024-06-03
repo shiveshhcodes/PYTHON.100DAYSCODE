d8e409483b7ab9e9deb5c8ca9515d14c1da9645b
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-04 04:38:58 +0530

    Update second.py

--- Tkinter Learning/second.py
+++ Tkinter Learning/second.py
@@ -7,5 +7,14 @@ window.attributes('-alpha',0.8) # this attribute is use to give transparency to 
 window.config(bg="green") # 1. as it shows , it uses to give background color 
 # window['bg']="yellow" # 2. as it shows , it uses to give background color 
 
+window.geometry("300x230+78+203") # here 300x230 is used for size of window and + x + y are used for loaction from left of window.
+# window.geometry("300x230-78-203") # here 300x230 is used for size of window and - x - y are used for loaction from right of window.
+
+# window.minsize(100,100) #use to maximize
+# window.maxsize(500,500) #use to minimize
+
+window.resizable(False,False) #use to fix the size and do not allow to resize it again and again
+
+
 window.mainloop()
-window.mainloop()
+
