7ef9b9db6e3b9b7dd986d6f0b8212a5c2e96ef29
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-04 04:38:56 +0530

    Update lazycode.py

--- Tkinter Learning/lazycode.py
+++ Tkinter Learning/lazycode.py
@@ -7,17 +7,17 @@ def ensure_py_extension(filename):
     return filename
 
 source_file=input("ENTER SOURCE FILE - ")
-copy_desination_file=input("ENTER DESTINATION FILE - ")
+copy_destination_file=input("ENTER DESTINATION FILE - ")
 
 source_file=ensure_py_extension(source_file)
-copy_desination_file=ensure_py_extension(copy_desination_file)
+copy_destination_file=ensure_py_extension(copy_destination_file)
 
 source = f"/Users/shiveshrichhariya/Desktop/GITHUB/100 DAYS OF CODE/Tkinter Learning/{source_file}"
-destination = f"/Users/shiveshrichhariya/Desktop/GITHUB/100 DAYS OF CODE/Tkinter Learning/{copy_desination_file}"
+destination = f"/Users/shiveshrichhariya/Desktop/GITHUB/100 DAYS OF CODE/Tkinter Learning/{copy_destination_file}"
  
 destination_dir = os.path.dirname(destination)
 if not os.path.exists(destination_dir):
         os.makedirs(destination_dir) 
 
-# if os.path.exists(source):
-shutil.copy(source , destination)
+if os.path.exists(source_file):
+ shutil.copy(source , destination) 
