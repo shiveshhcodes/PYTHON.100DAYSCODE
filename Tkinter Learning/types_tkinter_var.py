4dbc87454c15523e50f6b7fef458f5513c01dc83
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-05 01:03:09 +0530

    Update types_tkinter_var.py

--- Tkinter Learning/types_tkinter_var.py
+++ Tkinter Learning/types_tkinter_var.py
@@ -3,8 +3,8 @@ win=Tk()
 
 # var = StringVar(win,value="python",name="2") #variables in string value.
 # var = IntVar(win,value="12.33") #variables in integer value.
-var = BooleanVar(win,value=True) #it is specifically designed to hold boolean values (True or False)
-# var.set()
+# var = BooleanVar(win,value=True) #it is specifically designed to hold boolean values (True or False)
+var = DoubleVar(win,value=12.34) #it is specifically used to give float values , with integer vakue as well
 
 print(var.get())
 
